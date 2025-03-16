6e6433f658b4fa48109de2096bd2d53c2e3671a1
osuiso-depot <85674477+osuiso-depot@users.noreply.github.com>
2025-03-16 14:02:12 +0900

    update 1 file and delete 1 file

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -102,6 +102,10 @@ function set_workflow(){
     wget -q -O "${target_dir}/AniDiff.json" "$workflow_url"
     workflow_url="https://raw.githubusercontent.com/osuiso-depot/docker_comfyui/refs/heads/main/config/workflows/AniDiff_ipa(remote).json"
     wget -q -O "${target_dir}/AniDiff_ipa(remote).json" "$workflow_url"
+    workflow_url="https://raw.githubusercontent.com/osuiso-depot/docker_comfyui/refs/heads/main/config/workflows/inspyrenet-rembg-basic-workflow.json"
+    wget -q -O "${target_dir}/inspyrenet-rembg-basic-workflow.json" "$workflow_url"
+    workflow_url="https://raw.githubusercontent.com/osuiso-depot/docker_comfyui/refs/heads/main/config/workflows/workflow-cntrlnet(vid-bg_test).json"
+    wget -q -O "${target_dir}/workflow-cntrlnet(vid-bg_test).json" "$workflow_url"
     if [ $? -ne 0 ]; then
         echo "Failed to download workflow"
     else
